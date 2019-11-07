a34b46b61388a259e4e83944f344de9546ddb126
정재현 <ray1203@naver.com>
2019-11-07 23:52:20 +0900

    죽어랏

--- FRAMEW~1/Framework/TestScene2.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "TestScene2.h"
-
-void TestScene2::Initialize()
-{
-	GameObject* g = PushBackGameObject(new GameObject(L"c.png"));
-	g->transform->SetPosition(300.0f, 400.0f);
-
-}
--- FRAMEW~1/Framework/TestScene2.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Scene.h"
-class TestScene2 :
-	public Scene
-{
-public:
-	virtual void Initialize();
-};
-
